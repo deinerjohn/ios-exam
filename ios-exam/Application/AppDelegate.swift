33f1891be3c3c207b1b90e26edce034b881e9ace
Deiner John <djcalbang@gmail.com>
2022-04-18 15:53:07 +0800

    construct folder into Clean architecture pattern

---
+++ ios-exam/Application/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  ios-exam
+//
+//  Created by Deiner John Calbang on 4/18/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let vc = ViewController()
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UINavigationController(rootViewController: vc)
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+}
+
